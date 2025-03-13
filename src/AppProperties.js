28951d5baee4fad114f68606d596551edee4c622
Abdul <haleemshaikh324@gmail.com>
2025-03-13 12:46:48 +0530

    abdulhaleem

--- src/AppProperties.js
+++ src/AppProperties.js
@@ -1,5 +1,4 @@
 export const AppProperties={
 // loca:"https://portfolio-server-mu-gray.vercel.app"
 loca:"https://personal-portfolio-git-master-haleemshaikhs-projects.vercel.app"
-// loca:"http://localhost:5000"
 }
