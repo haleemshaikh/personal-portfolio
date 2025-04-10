ae5c22f4f1308eba73b07397139f6b650e46cffd
Abdul <haleemshaikh324@gmail.com>
2025-04-10 12:16:19 +0530

    delete vercel.json resume modified AppProperties.js

--- src/AppProperties.js
+++ src/AppProperties.js
@@ -1,4 +1,3 @@
 export const AppProperties={
-// loca:"https://portfolio-server-mu-gray.vercel.app"
-loca:"https://personal-portfolio-git-master-haleemshaikhs-projects.vercel.app"
+loca:"https://portfolio-backend-phi-amber.vercel.app"
 }
